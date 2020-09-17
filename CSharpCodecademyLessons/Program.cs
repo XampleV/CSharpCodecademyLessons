41969a21204caf58af21e32387abecac13e3a9df
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-09-17 08:20:10 -0400

    Add project files.

---
+++ CSharpCodecademyLessons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpCodecademyLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
