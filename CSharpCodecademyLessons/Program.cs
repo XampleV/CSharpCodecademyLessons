7870708c671f59b4459b57b526e3e3d9ae7ff8c9
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-09-17 08:23:52 -0400

    money maker

--- CSharpCodecademyLessons/Program.cs
+++ CSharpCodecademyLessons/Program.cs
@@ -6,7 +6,23 @@ namespace CSharpCodecademyLessons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MoneyMaker();
+        }
+        static void MoneyMaker()
+        {
+            
+            Console.WriteLine("Welcome to Money Maker! Enter an amount to convert to coins:");
+            double amount = Convert.ToDouble(Console.ReadLine());
+            double goldCoinsValue = 10;
+            double silverCoinsValue = 5;
+            double goldCoins = Math.Floor(amount / goldCoinsValue);
+            double remainderAfterGold = amount % goldCoinsValue;
+            double silverCoins = Math.Floor(remainderAfterGold / silverCoinsValue);
+            double remainder = amount % silverCoinsValue;
+            Console.WriteLine($"{amount} cents is equal to ...");
+            Console.WriteLine($"Gold Coins: {goldCoins}");
+            Console.WriteLine($"silver Coins: {silverCoins}");
+            Console.WriteLine($"Bronze Coins: {remainder}");
         }
     }
 }
